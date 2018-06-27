1bea6818930627fd84c0075602020483ff3f349c
김강희 <ganghe74@kookmin.ac.kr>
2018-06-27 18:27:26 +0900

    2445_별 찍기 - 8

---
+++ C++/2445_별 찍기 - 8.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+int main() {
+	int N;
+	scanf("%d", &N);
+	for (int i=0;i<N;i++){
+		for (int j=0;j<2*N;j++){
+			if (j<=i || j>=(2*N-1-i)) printf("*");
+			else printf(" ");
+		}
+		printf("\n");
+	}
+	for (int i=N-2;i>=0;i--){
+		for (int j=0;j<2*N;j++){
+			if (j<=i || j>=(2*N-1-i)) printf("*");
+			else printf(" ");
+		}
+		printf("\n");
+	}
+}
