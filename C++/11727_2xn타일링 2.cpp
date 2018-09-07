4ef851789676ac01d5ad0e8c2855189f7604dbfa
ganghe74 <ganghe74@kookmin.ac.kr>
2018-09-08 00:12:39 +0900

    11727_2xn타일링 2

---
+++ C++/11727_2xn타일링 2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int d[1001] = {0,1,3};
+int solve(int x) {
+	if (d[x] > 0) return d[x];
+	d[x] = (solve(x-2) * 2 + solve(x-1)) % 10007;
+	return d[x];
+}
+int main() {
+	int n;
+	cin >> n;
+	cout << solve(n);
+}
