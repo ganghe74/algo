9ef8a11043e1513f3db62c572d01a5d4e3b97adf
ganghe74 <ganghe74@kookmin.ac.kr>
2018-09-08 09:51:00 +0900

    combine solutions to one file

--- C++/1463_1로 만들기.cpp
+++ C++/1463_1로 만들기.cpp
@@ -1,3 +1,5 @@
+/*
+1.Top down
 #include <iostream>
 using namespace std;
 int d[1000001] = {0};
@@ -18,3 +20,27 @@ int main() {
 	cin >> N;
 	cout << solve(N);
 }
+*/
+
+
+
+/*
+2.Bottom up
+#include <iostream>
+using namespace std;
+int main() {
+	int N;
+	cin >> N;
+	int d[N+1];
+	for (int i=2;i<=N;++i) {
+		d[i] = d[i-1]+1;
+		if (i%2 == 0) {
+			if (d[i/2] + 1 < d[i]) d[i] = d[i/2] + 1;
+		}
+		if (i%3 == 0) {
+			if (d[i/3] + 1 < d[i]) d[i] = d[i/3] + 1;
+		}
+	}
+	cout << d[N];
+}
+*/
--- C++/2747_피보나치 수.cpp
+++ C++/2747_피보나치 수.cpp
@@ -1,3 +1,5 @@
+/*
+1.Top down
 #include <iostream>
 using namespace std;
 int memo[46];
@@ -16,3 +18,21 @@ int main() {
 	cin >> x;
 	cout << fibonacci(x);
 }
+*/
+
+
+
+/*
+2.Bottom up
+#include <iostream>
+using namespace std;
+int main() {
+	int n;
+	int fibo[46] = {0,1,1};
+	cin >> n;
+	for (int i=3;i<=n;++i){
+		fibo[i] = fibo[i-1] + fibo[i-2];
+	}
+	cout << fibo[n];
+}
+*/
