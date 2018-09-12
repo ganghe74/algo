e9641b4ed4fd61986b9c8a6eead1bce384afc9d5
ganghe74 <ganghe74@kookmin.ac.kr>
2018-09-12 23:53:44 +0900

    1934_최소공배수

---
+++ C++/1934_최소공배수.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int gcd(int a, int b) {	
+	while(b!=0) {
+		int r = a%b;
+		a = b;
+		b = r;
+	}
+	return a;
+}
+int lcm(int a, int b) {
+	return a / gcd(a, b) * b;
+}
+int main() {
+	int T;
+	cin >> T;
+	while (T--) {
+		int A, B;
+		cin >> A >> B; 
+		cout << lcm(A,B) << endl;
+	}
+}
