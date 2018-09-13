7c53f67c868bbbba5db1b735c179deab948b6869
ganghe74 <ganghe74@kookmin.ac.kr>
2018-09-13 22:30:18 +0900

    :GOD:
    solved by :GOD:
    이 문서는 4차원적인 것을 다룹니다.
    이 문서에서 설명하는 대상은 우리가 경험해본 4차원 그 자체거나, 혹은 관련된 것이거나,
    일반인이 이해하기 힘든 희한한 정신세계를 가진 사람에 대해 서술합니다.
    후자의 경우 또라이, 사이코 끼가 있을 수도 있습니다.

--- C++/1676_팩토리얼 0의 개수.cpp
+++ C++/1676_팩토리얼 0의 개수.cpp
@@ -3,12 +3,8 @@ using namespace std;
 int main() {
 	int N, answer = 0;
 	cin >> N;
-	for (int i=5;i<=N;i+=5) {
-		int temp = i;
-		while (temp % 5 == 0) {
-			answer += 1;
-			temp = temp / 5;
-		}
+	for (int i=5;i<=N;i*=5) {
+		answer += N/i;
 	}
 	cout << answer;
 }
