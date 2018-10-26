6145d32c22d8a937b9d43ab50afdc3be21b9dbb7
ganghe74 <ganghe74@kookmin.ac.kr>
2018-10-26 23:11:05 +0900

    1167_트리의 지름

---
+++ C++/1167_트리의 지름.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cstring>
+#include <queue>
+using namespace std;
+struct Edge {
+    int to;
+    int cost;
+    Edge(int to, int cost) : to(to), cost(cost) {
+    }
+};
+vector<Edge> a[100001];
+int dist[100001];
+int bfs(int x) {
+    memset(dist, -1, sizeof(dist));
+    dist[x] = 0;
+    queue<int> q;
+    q.push(x);
+    while (!q.empty()) {
+        int now = q.front();
+        q.pop();
+        for (Edge e : a[now]) {
+            if (dist[e.to] == -1) {
+                q.push(e.to);
+                dist[e.to] = dist[now] + e.cost;
+            }
+        }
+    }
+}
+int main() {
+    int V;
+    scanf("%d", &V);
+    for (int i=1;i<=V;++i) {
+        int x, y, d;
+        scanf("%d", &x);
+        while (true) {
+            scanf("%d", &y);
+            if (y == -1) break;
+            scanf("%d", &d);
+            a[x].push_back(Edge(y,d));
+        }
+    }
+    bfs(1);
+    int start = 1;
+    for (int i=1;i<=V;++i) {
+        if (dist[i] > dist[start]) start = i;
+    }
+    bfs(start);
+    int diameter = 0;
+    for (int i=1;i<=V;++i) {
+        if (dist[i] > diameter) diameter = dist[i];
+    }
+    printf("%d", diameter);
+}
