b2fdaace2fbb007271d00b7636dbf5c8ba87e391
ganghe74 <ganghe74@kookmin.ac.kr>
2018-10-31 01:05:34 +0900

    1427_소트인사이드

---
+++ C++/1427_소트인사이드.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main() {
+    string N;
+    cin >> N;
+    sort(N.begin(), N.end(), greater<int>());
+    cout << N;
+}
