e34bb463710d75b664ad3f887da5df955fde826a
ganghe74 <ganghe74@kookmin.ac.kr>
2018-11-03 18:34:51 +0900

    6_BOARDCOVER

--- algospot/6_BOARDCOVER.cpp
+++ algospot/6_BOARDCOVER.cpp
@@ -12,9 +12,9 @@ const int coverType[4][3][2] = {
 
 bool set(vector<vector<int>>& board, int y, int x, int type, int delta) {
     bool ok = true;
-    for (int i=0;i<3;++i) {
-        const int ny = y + coverType[type][i][0];
-        const int nx = x + coverType[type][i][1];
+    for (int k=0;k<3;++k) {
+        const int ny = y + coverType[type][k][0];
+        const int nx = x + coverType[type][k][1];
         if (ny < 0 || ny >= board.size() || nx < 0 || nx >=board[0].size()) ok = false;
         else if((board[ny][nx] += delta) > 1) ok = false;
     }
@@ -25,15 +25,20 @@ int cover(vector<vector<int>> &board) {
     int y = -1, x = -1;
     for (int i=0;i<board.size();++i) {
         for (int j=0;j<board[0].size();++j) {
-            y = i;
-            x = j;
-            break;
+            if (!board[i][j]) {
+                y = i;
+                x = j;
+                break;
+            }
         }
+        if (y != -1) break;
     }
     if (y == -1) return 1;
     int cnt = 0;
     for (int k=0;k<4;++k) {
-        if (set(board,y,x,k,1)) cnt += cover(board);
+        if (set(board,y,x,k,1)) {
+            cnt += cover(board);
+        }
         set(board,y,x,k,-1);
     }
     return cnt;
@@ -43,6 +48,7 @@ int main() {
     int C;
     scanf("%d", &C);
     while (C--) {
+    
         int H, W;
         scanf("%d %d", &H, &W);
         for (int i=0;i<H;++i) {
