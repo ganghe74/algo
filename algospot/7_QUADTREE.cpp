932abc5b3d44eeb1d791d06aad29a4a578cec4fb
ganghe74 <ganghe74@kookmin.ac.kr>
2018-11-04 17:35:38 +0900

    7_QUADTREE
    반복자가뭘까...

---
+++ algospot/7_QUADTREE.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+string reverse(string::iterator& it) {
+    char head = *it;
+    ++it;
+    if(head == 'b' || head == 'w') return string(1, head);
+    string upperLeft = reverse(it);
+    string upperRight = reverse(it);
+    string lowerLeft = reverse(it);
+    string lowerRight = reverse(it);
+    return string("x") + lowerLeft + lowerRight + upperLeft + upperRight;
+}
+int main() {
+    int C;
+    cin >> C;
+    while (C--) {
+        string in;
+        cin >> in;
+        string::iterator it = in.begin();
+        cout << reverse(it); << endl;
+    }
+}
