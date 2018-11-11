8729e107512de547d3788981fa665e16abce2459
ganghe74 <ganghe74@kookmin.ac.kr>
2018-11-11 22:51:30 +0900

    4504_배수 찾기.cpp

---
+++ baekjoon/4504_배수 찾기.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int n, num;
+    scanf("%d", &n);
+    scanf("%d", &num);
+    while (num != 0) {
+        if (num % n == 0) printf("%d is a multiple of %d.\n", num, n);
+        else printf("%d is NOT a multiple of %d.\n", num, n);
+        scanf("%d", &num);
+    }
+}
