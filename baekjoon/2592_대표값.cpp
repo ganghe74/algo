29e3e573168921e734cd1d01c7f0646a61e7931d
ganghe74 <ganghe74@kookmin.ac.kr>
2018-11-20 23:04:17 +0900

    fix

--- baekjoon/2592_대표값.cpp
+++ baekjoon/2592_대표값.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-int count[100];
+int count[100] = {0};
 int main() {
     int a[10], sum = 0;
     for (int i=0;i<10;++i) {
@@ -8,10 +8,9 @@ int main() {
         sum += a[i];
         count[a[i]/10] += 1;
     }
-    printf("%d\n", sum/10);
     int most = 0;
-    for (int i=0;i<10;++i) {
+    for (int i=0;i<100;++i) {
         if (count[most] < count[i]) most = i;
     }
-    printf("%d", most*10);
+    printf("%d\n%d", sum/10, most*10);
 }
