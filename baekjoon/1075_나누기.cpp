73bff1e7898585272470e8714ecacfee63c94170
ganghe74 <ganghe74@kookmin.ac.kr>
2018-11-25 02:15:30 +0900

    1075_나누기

---
+++ baekjoon/1075_나누기.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int N, F;
+    scanf("%d%d", &N, &F);
+    N -= N % 100;
+    for (int i=0;i<100;++i) {
+        if ((N+i) % F == 0) {
+            printf("%02d", i);
+            return 0;
+        }
+    }
+}
