c9a3ae8ca4d8a043586ad6a159a00e54c3f0f992
ganghe74 <ganghe74@kookmin.ac.kr>
2018-11-28 23:17:20 +0900

    1037_약수

---
+++ baekjoon/1037_약수.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int N;
+    scanf("%d", &N);
+    int a[N];
+    for (int i=0;i<N;++i) scanf("%d", &a[i]);
+    sort(a, a+N);
+    int answer = a[0] * a[N-1];
+    printf("%d", answer);
+}
