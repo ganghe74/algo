3d6449763bb75f00c91886deac23d70b6082df4b
ganghe74 <ganghe74@kookmin.ac.kr>
2019-01-01 18:53:26 +0900

    5554_심부름 가는 길

---
+++ baekjoon/5554_심부름 가는 길.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b, c, d;
+    scanf("%d%d%d%d", &a, &b, &c, &d);
+    int sum = a + b + c + d;
+    printf("%d\n%d", sum/60, sum%60);
+}
