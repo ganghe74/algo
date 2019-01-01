81440ee5b35b8d743319838d01696b6e39723311
ganghe74 <ganghe74@kookmin.ac.kr>
2019-01-01 18:47:49 +0900

    2490_윷놀이
    koosaga edition

---
+++ baekjoon/2490_윷놀이_2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#define h a+b+c+d
+int main() {
+    int a, b, c, d;
+    char yoot[5] = {'D', 'C', 'B', 'A', 'E'};
+    for (int i=0;i<3;++i) {
+        scanf("%d%d%d%d", &a, &b, &c, &d);
+        printf("%c\n", yoot[h]);
+    }
+}
