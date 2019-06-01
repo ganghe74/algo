8c51d068a92702d574aabeb477f9c375476b93ef
ganghe74 <ganghe74@kookmin.ac.kr>
2019-06-01 23:51:33 +0900

    fastscan
    Inspired by boj 11921

--- algorithm/FastIO.cpp
+++ algorithm/FastIO.cpp
@@ -16,8 +16,36 @@ void fastscan(int &x) {
         x *= -1;
 }
 
+static char _buffer[524288];
+static unsigned _currentChar = 0;
+  
+static inline int _read() {
+    if (_currentChar == 524288) {
+        fread(_buffer, 1, 524288, stdin);
+        _currentChar = 0;
+    }
+    return _buffer[_currentChar++];
+}
+  
+unsigned c, x;
+  
+static inline unsigned _readInt() {
+    c = _read();
+    while (c <= 32) c = _read();
+    x = 0;
+    while (c > 32) {
+        x *= 10;
+        x += c - '0';
+        c = _read();
+    }
+    return x;
+}
+
 int main() {
     int a;
     fastscan(a);
+    printf("%d\n", a);
+    
+    a = _readInt();
     printf("%d\n", a);
 }
