d46f39b29a32957a4544925345445f3dd8fa1e9b
ganghe74 <ganghe74@kookmin.ac.kr>
2019-12-22 22:26:10 +0900

    Short KMP

--- algorithm/KMP.cpp
+++ algorithm/KMP.cpp
@@ -22,6 +22,16 @@ vector<int> getPartialMatch(const string& N) {
     return pi;
 }
 
+vector<int> getPartialMatch2(const string &N) {
+    int m = N.size();
+    vector<int> pi(m, 0);
+    for (int i=1,j=0;i<m;++i) {
+        while (j && N[i] != N[j]) j = pi[j-1];
+        if (N[i] == N[j]) pi[i] = ++j;
+    }
+    return pi;
+}
+
 vector<int> kmpSearch(const string& H, const string &N) {
     int n = H.size(), m = N.size();
     vector<int> ret;
@@ -58,6 +68,23 @@ vector<int> kmpSearch2(const string& H, const string& N) {
                 ret.push_back(i - m + 1);
                 matched = pi[matched-1];
             }
+        }
+    }
+    return ret;
+}
+
+vector<int> kmpSearch3(const string &H, const string &N) {
+    int n = H.size(), m = N.size();
+    vector<int> ret;
+    vector<int> pi = getPartialMatch(N);
+    for (int i=0,j=0;i<n;++i) {
+        while (j && H[i] != N[j]) j = pi[j-1];
+        if (H[i] == N[j]) {
+            if (j == m-1) {
+                ret.push_back(i - m + 1);
+                j = pi[j];
+            }
+            else j++;
         }
     }
     return ret;
