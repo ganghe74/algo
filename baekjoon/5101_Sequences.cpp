c5970186d3e5921be3c98be8340d32cb5545d2f7
ganghe74 <ganghe74@kookmin.ac.kr>
2020-03-17 04:18:08 +0900

    rejudge

--- baekjoon/5101_Sequences.cpp
+++ baekjoon/5101_Sequences.cpp
@@ -4,9 +4,9 @@ int main() {
     int a, b, c;
     while (true) {
         scanf("%d%d%d", &a, &b, &c);
-        if (a == 0 && a == b && b == c) break;
+        if (!a && !b && !c) break;
         c -= a;
-        if (c % b == 0) printf("%d\n", c/b+1);
+        if (c % b == 0 && c >= 0) printf("%d\n", c/b+1);
         else puts("X");
     }
 }
